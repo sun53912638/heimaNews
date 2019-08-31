5f246abbddcb1e9610a2b5915c7ddb894a9e4257
sun53912638 <53866851+sun53912638@users.noreply.github.com>
2019-09-01 01:21:27 +0800

    黑马头条PC-登录模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,33 +6,30 @@
             <img src="../../assets/img/logo_index.png" alt="">
         </div>
         <!-- 表单组件 容器-->
-        <el-form style = 'margin-top:20px'>
+        <!-- 记得给容器绑定:model,:rules,这是规定 -->
+        <el-form ref="loginFrom" :model="loginFrom" style = 'margin-top:20px' :rules="loginRules">
             <!-- 表单项 -->
-            <el-form-item>
+            <el-form-item prop="tell" >
                 <!-- 放置组件内容 -->
-                <el-input ></el-input>
+                <el-input v-model="loginFrom.tell" placeholder="请输入正确的手机号"></el-input>
             </el-form-item>
-        </el-form>
-        <!-- 表单组件 容器-->
-        <el-form style = 'margin-top:20px '>
+
             <!-- 表单项 -->
-            <el-form-item>
+            <el-form-item prop="code">
                 <!-- 放置组件内容 -->
-                <el-input style="width:220px"></el-input>
+                <el-input v-model="loginFrom.code" placeholder="请输入验证码" style="width:220px"></el-input>
                  <el-button  style="float:right">发送验证码</el-button>
             </el-form-item>
-        </el-form>
-        <!-- 表单组件 容器-->
-        <el-form style = 'margin-top:20px'>
+
             <!-- 表单项 -->
-            <el-form-item>
+            <el-form-item prop="check">
                 <!-- 放置组件内容 -->
-                <el-checkbox>我已阅读并同意协议和隐私条款</el-checkbox>
+                <el-checkbox v-model="loginFrom.check">我已阅读并同意协议和隐私条款</el-checkbox>
             </el-form-item>
             <!-- 确定按钮 -->
-            <el-from-item>
-                <el-button type="primary" style="width:100%">登录</el-button>
-            </el-from-item>
+            <el-form-item>
+                <el-button @click="add" type="primary" style="width:100%">登录</el-button>
+            </el-form-item>
         </el-form>
     </el-card>
 
@@ -41,6 +38,55 @@
 
 <script>
 export default {
+  data () {
+    let validator = function (rule, value, callBack) {
+      if (value) {
+        callBack()
+      } else {
+        callBack(new Error('您必须同意'))
+      }
+    }
+    return {
+      loginFrom: {
+        tell: '',
+        code: '',
+        check: true
+      }, // 制定表单校验规则
+      loginRules: {
+        tell: [{
+          required: true,
+          message: '手机号不能为空'
+        }, {
+          pattern: /^1[3456789]\d{9}$/,
+          message: '手机号不正确'
+        }],
+        code: [{
+          required: true,
+          message: '验证不能为空'
+        }, {
+          pattern: /^\d{6}$/,
+          message: '验证不能为空'
+        }],
+        check: [{// 要用自定义校验函数
+          validator
+        }]
+      }
+    }
+  },
+  methods: {
+    add () {
+      this.$refs.loginFrom.validate((isOK, obj) => {
+        if (isOK) {
+          this.$message({ type: 'sucsess', message: '成功' })
+        } else {
+          this.$message({ type: 'warning', message: '失败' })
+        }
+      })
+    }
+  },
+  mounted () {
+
+  }
 
 }
 </script>
