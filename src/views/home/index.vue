5dd75e2095b0f4b4856745eae66373902df5f548
sun53912638 <53866851+sun53912638@users.noreply.github.com>
2019-09-02 10:31:08 +0800

    黑马头条PC-主页模块-主页布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,22 @@
 <template>
-  <div>
-      home
-  </div>
+<!-- 最外侧 -->
+<el-container>
+  <!-- 左侧 -->
+  <el-aside>
+    左侧
+  </el-aside>
+  <!-- 右侧 -->
+  <el-container>
+    <!-- 顶栏 -->
+  <el-header>
+    顶部
+  </el-header>
+  <!-- 中间 -->
+  <el-main>
+    中间
+  </el-main>
+  </el-container>
+</el-container>
 </template>
 
 <script>
