c25a3845bc6266c4d804df646a46d1ca68a45a6b
sun53912638 <53866851+sun53912638@users.noreply.github.com>
2019-09-03 21:20:09 +0800

    黑马头条PC-评论列表-页面结构-table的使用

--- src/compnents/common/breadCrumb.vue
+++ src/compnents/common/breadCrumb.vue
@@ -3,7 +3,7 @@
     <el-breadcrumb-item :to="{path:'/'}">首页</el-breadcrumb-item>
     <el-breadcrumb-item>
          <!-- 定义一个插槽 -->
-         <slot name="title"></slot>
+         <slot name="title">评论列表</slot>
     </el-breadcrumb-item>
 
   </el-breadcrumb>
--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>评论列表</div>
+  <!-- 最外层用el-card卡面 -->
+  <el-card>
+    <bread-crumb slot="header">
+      <template slot="title">评论列表</template>
+    </bread-crumb>
+    <el-table :data="tableData">
+      <el-table-column width="700" prop="date" label="标题">标题</el-table-column>
+      <el-table-column prop="name" label="评论状态">评论状态</el-table-column>
+      <el-table-column prop="address" label="总评论数">总评论数</el-table-column>
+      <el-table-column prop="address" label="粉丝评论数">粉丝评论数</el-table-column>
+      <el-table-column prop="address" label="操作">操作</el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      list: []
+    }
+  },
+  methods: {
 
+  }
 }
 </script>
 
