203116ca6443e2eba7bb0e8d907074853c1a7279
sun53912638 <53866851+sun53912638@users.noreply.github.com>
2019-09-05 17:51:13 +0800

    黑马头条PC-素材列表-页面结构-tab

---
+++ src/views/material/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-card>
+    <bread-crumb slot="header">
+      <template slot="title">素材管理</template>
+    </bread-crumb>
+    <el-tabs type="card" @tab-click="changeTab">
+      <el-tab-pane label="全部素材" name="first">
+        <!-- 全部素材 -->
+      </el-tab-pane>
+      <el-tab-pane label="收藏内容" name="second">
+        <!-- 收藏内容 -->
+      </el-tab-pane>
+    </el-tabs>
+  </el-card>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      activeName: 'first',
+      list: []
+    }
+  },
+  methods: {
+    changeTab () {
+
+    },
+    getMaterial () {
+      this.$http({
+        url: '/articles',
+        prama
+      })
+    }
+  }
+}
+</script>
+
+<style>
+</style>
