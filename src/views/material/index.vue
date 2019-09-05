7fe39dd0e505cf0dbab3ecf7206729ee4396c787
sun53912638 <53866851+sun53912638@users.noreply.github.com>
2019-09-05 18:56:02 +0800

    黑马头条PC-素材列表-数据加载

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,14 +1,28 @@
 <template>
-  <el-card>
+  <el-card class="material">
     <bread-crumb slot="header">
       <template slot="title">素材管理</template>
     </bread-crumb>
-    <el-tabs type="card" @tab-click="changeTab">
-      <el-tab-pane label="全部素材" name="first">
+    <el-tabs type="card" v-model="activeName" @tab-click="changeTab">
+      <el-tab-pane label="全部素材" name="all">
         <!-- 全部素材 -->
+        <div class="card-list">
+          <el-card class="img-card" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt />
+            <el-row class="operate" type="flex" align="middle" justify="space-around">
+              <i :style="{color: item.is_collected ? 'yellow' : '#266'}" class="el-icon-star-on"></i>
+              <i class="el-icon-delete-solid"></i>
+            </el-row>
+          </el-card>
+        </div>
       </el-tab-pane>
-      <el-tab-pane label="收藏内容" name="second">
+      <el-tab-pane label="收藏内容" name="collect">
         <!-- 收藏内容 -->
+        <div class="card-list">
+          <el-card class="img-card" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt />
+          </el-card>
+        </div>
       </el-tab-pane>
     </el-tabs>
   </el-card>
@@ -18,23 +32,56 @@
 export default {
   data () {
     return {
-      activeName: 'first',
+      activeName: 'all',
       list: []
     }
   },
   methods: {
     changeTab () {
-
+      this.getMaterial()
     },
     getMaterial () {
       this.$http({
-        url: '/articles',
-        prama
+        url: '/user/images',
+        params: {
+          collect: this.activeName === 'collect' // 为true查询全部数据,为false查询收藏数据
+        }
+      }).then(res => {
+        this.list = res.data.results
       })
     }
+  },
+  created () {
+    this.getMaterial()
   }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.material {
+  .card-list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    .img-card {
+      width: 180px;
+      height: 180px;
+      margin: 20px;
+      position: relative;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+      .operate {
+        position: absolute;
+        width: 100%;
+        height: 30px;
+        left: 0;
+        bottom: 0;
+        font-size: 18px;
+        color: #266;
+      }
+    }
+  }
+}
 </style>
