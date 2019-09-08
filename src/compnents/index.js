fa92ee357a63fb3a2e1697af6d90721f5075006a
sun53912638 <53866851+sun53912638@users.noreply.github.com>
2019-09-08 10:02:15 +0800

    黑马头条PC-发布文章-注册富文本编辑器及应用

--- src/compnents/index.js
+++ src/compnents/index.js
@@ -2,11 +2,18 @@
 import leftAside from './home/leftaside.vue'
 import layoutHeader from './home/layout-header.vue'
 import breadCrumb from './common/breadCrumb.vue'
+// require styles
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+import { quillEditor } from 'vue-quill-editor'
 
 export default {
   install: function (Vue) {
     Vue.component('left-aside', leftAside) // 注册左侧导航组件
     Vue.component('layout-header', layoutHeader)
     Vue.component('bread-crumb', breadCrumb)
+    Vue.component('quill-editor', quillEditor)
   }
 }
