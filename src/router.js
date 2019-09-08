4e1667e26ae3f35863b721fe333fddb095fbf1cf
sun53912638 <53866851+sun53912638@users.noreply.github.com>
2019-09-08 22:46:16 +0800

    黑马头条PC-账户信息-新增页面-挂载路由

--- src/router.js
+++ src/router.js
@@ -37,6 +37,9 @@ export default new Router({
       }, {
         path: 'publish',
         component: () => import('./views/publish')
+      }, {
+        path: 'account',
+        component: () => import('./views/account')
       }]
     }
 
